f2178f949f436413f02976e2e5f03718415f8f84
Brandon Chan <iambrandonchan@gmail.com>
2019-03-25 21:14:18 -0500

    added note, tone, and chord class

--- midiAnalyzer.py
+++ midiAnalyzer.py
@@ -1,12 +1,55 @@
 from mido import MidiFile
+from mido.midifiles.units import tempo2bpm, bpm2tempo, tick2second, second2tick
+from midiUtils import Utils, Tone, Note, Chord
 
+
+
+
+
+
+
+util = Utils()
 #note on my keyboard is from note=21 to note=108 == 88 keys
 
-# mid = MidiFile('./aFriendLikeYou/aflu1.mid')
-mid = MidiFile('test.mid')
-
-
+# mid = MidiFile('./aFriendLikeYou/aflu6.mid')
+mid = MidiFile('legato.mid')
+print((mid.ticks_per_beat))
+# print(mid.ticks_per_beat, 1000)
+# print(mid.tracks)
+# exit()
+tempoPerMinute = 500000
+totalNotes = list()
 for i, track in enumerate(mid.tracks):
     print('Track {}: {}'.format(i, track.name))
+    # print(dir(track))
+    cumulative_time = 0
+    currently_played_notes = dict()
+
     for msg in track:
-        print(msg)
+    	if not msg.is_meta:
+    		# print(msg, tick2second(msg.time, mid.ticks_per_beat, tempoPerMinute), util.note_number_to_letter(msg.note))
+    		cumulative_time += tick2second(msg.time, mid.ticks_per_beat, tempoPerMinute)
+    		message_note = util.note_number_to_letter(msg.note)
+    		if message_note in currently_played_notes:
+    			absoluteStart, velocityStart = currently_played_notes.pop(message_note)
+    			note = Note(absoluteStart, cumulative_time, message_note, velocityStart, msg.velocity)
+    			totalNotes.append(note)
+    		else:
+    			currently_played_notes[message_note] = (cumulative_time, msg.velocity)
+
+
+    for note in totalNotes:
+    	print(note.notePlayed, note.absoluteStart, note.absoluteStop)
+
+    		# print(util.note_number_to_letter(msg.note), cumulative_time)
+    		# print(msg)
+
+    	# print(dir(msg))
+    	# print(msg.is_realtime)
+    	# exit()
+    	# if not msg.is_meta:
+        	# print(util.note_number_to_letter(msg.note))
+        	# print(msg, util.note_number_to_letter(msg.note))
+
+
+
--- midiUtils.py
+++ midiUtils.py
@@ -1,21 +1,91 @@
-number_to_letter_mappings = dict()
+class Utils:
+	def __init__(self):
+		self.number_to_letter_mappings = dict()
+		self.pattern = ['A', 'A#/Bb', 'B', 'C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab']
 
-pattern = ['A', 'A#/Bb', 'B', 'C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab']
+	def build_number_letter_mapping(self):
+		if len(self.number_to_letter_mappings) == 0:
+			octave = 0
+			for num_note in range(21, 109):
+				letter = self.pattern[(num_note - 21) % len(self.pattern)]
+				if letter == 'C':
+					octave += 1
+				self.number_to_letter_mappings[num_note] = self.pattern[(num_note - 21) % len(self.pattern)] + str(octave)
 
-def build_number_letter_mapping():
-	if len(number_to_letter_mappings) == 0:
-		octave = 0
-		for num_note in range(21, 109):
-			letter = pattern[(num_note - 21) % len(pattern)]
-			if letter == 'C':
-				octave += 1
-			number_to_letter_mappings[num_note] = pattern[(num_note - 21) % len(pattern)] + str(octave)
+	def note_number_to_letter(self, num_note):
+		self.build_number_letter_mapping()
+		return self.number_to_letter_mappings[num_note]
 
 
-def note_number_to_letter(num_note):
-	build_number_letter_mapping()
-	return number_to_letter_mappings[num_note]
 
-for i in range(21, 2019):
-	print (note_number_to_letter(i))
+class Tone:
+	def __init__(self):
+		self.threshold_limit = .25
 
+	def is_Note(self):
+		return False
+	def is_Chord(self):
+		return False
+	def is_close(self, other_note):
+		raise NotImplementedError("To be implemented")
+
+class Note(Tone):
+
+	def __init__(self, absoluteStart, absoluteStop, notePlayed, velocityStart, velocityStop):
+		super().__init__()
+		self.absoluteStart = absoluteStart
+		self.absoluteStop = absoluteStop
+		self.duration = self.absoluteStop - self.absoluteStart
+		self.notePlayed = notePlayed
+		self.velocityStart = velocityStart
+		self.velocityStop = velocityStop
+
+	def is_note(self):
+		return True
+
+	def is_close(self, other_note):
+		start_difference = abs(self.absoluteStart - other_note.absoluteStart)
+		stop_difference = abs(self.absoluteStop - other_note.absoluteStop)
+		
+
+		if start_difference <= threshold_limit and stop_difference <= threshold_limit:
+			return True
+		return False
+
+class Chord(Tone):
+
+	def __init__(self):
+		super().__init__()
+		self.notes_in_chord = list()
+		self.num_chords = 0
+		self.average_duration = 0
+		self.average_start_time = 0
+		self.average_stop_time = 0
+
+
+	def is_chord(self):
+		return True
+
+	def add_note(self, note):
+		self.average_duration = self.get_new_average(self.average_duration, note.duration)
+		self.average_start_time = self.get_new_average(self.average_start_time, note.absoluteStart)
+		self.average_stop_time = self.get_new_average(self.average_stop_time, note.absoluteStop)
+
+		self.num_chords += 1
+
+		self.notes_in_chord.append(note)
+
+
+	def get_new_average(self, previous_cumulation, next_number):
+		previous_sum = previous_cumulation * self.num_chords
+		current_sum = previous_sum + next_number
+		return current_sum / (self.num_chords + 1)
+
+	def is_close(self, other_note):
+		start_difference = abs(self.average_start_time - other_note.absoluteStart)
+		stop_difference = abs(self.average_stop_time - other_note.absoluteStop)
+		
+
+		if start_difference <= threshold_limit and stop_difference <= threshold_limit:
+			return True
+		return False
